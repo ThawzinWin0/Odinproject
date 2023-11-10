9aa3c838146b05d00b7dc35fb81b282adbbeeede
ThawzinWin0 <150348662+ThawzinWin0@users.noreply.github.com>
2023-11-10 10:12:41 +0630

    Add files via upload

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,48 @@
+let todoList = [{
+ name:'make dinner', 
+ dueDate:'2023/12/22'
+},{
+  name: 'wash dishes',
+  dueDate:'2023/12/22'
+}];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = "";
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    const {name , dueDate} = todoObject;
+    const html = `
+    <div>${name}</div>
+    <div> ${dueDate}</div> 
+    <button onclick="
+     todoList.splice( ${i}, 1);
+     renderTodoList();
+    " class ="delet-todo-button">Delete</button>
+    
+    `;
+    todoListHTML += html;
+  }
+  
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
+  const dateInputElemet = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElemet.value;
+
+  todoList.push({
+    //name : name , dueDate: dueDate
+    name ,
+    dueDate  
+  });
+  
+
+  inputElement.value = "";
+  renderTodoList();
+}
